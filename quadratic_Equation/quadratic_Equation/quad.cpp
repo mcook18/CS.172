683aacf0848fea06e3f3b9c190ca25d7763dd126
mcook18 <mcnadine.cook@gmail.com>
2015-05-26 15:30:08 -0700

    Quadratic Equation
    Finished version

--- quadratic_Equation/quadratic_Equation/quad.cpp
+++ quadratic_Equation/quadratic_Equation/quad.cpp
@@ -2,69 +2,61 @@
 using namespace std;
 
 class QuadraticEquation{
-public:
+private:
 	int a;
 	int b;
 	int c;
+public:
 	int geta();
 	int getb();
 	int getc();
-	int getdescrim(int a,int b,int c);
-	double getroot1(int a, int b, int c);
-	double getroot2(int a, int b, int c);
+	int getdescrim();
+	double getroot1();
+	double getroot2();
 
-	QuadraticEquation(){
-		a = 0;
-		b = 0;
-		c = 0;
+	QuadraticEquation(int x,int y,int z){
+		a = x;
+		b = y;
+		c = z;
 	}
 
 };
 
-int geta();
-int getb();
-int getc();
-int getdescrim(int a, int b, int c);
-double getroot1(int a, int b, int c);
-double getroot2(int a, int b, int c);
-
-
-int main(){
-	QuadraticEquation EQ;
-
-	geta();
-	getb();
-	getc();
-	cout << getdescrim(a, b, c)
-}
-
-int geta(){
-	int a;
-	cout << "What number do you want A to be?";
-	cin >>a;
-	return a;
-}
-int getb(){
-	int b;
-	cout << "What number do you want B to be?";
-	cin >> b;
-	return b;
-}
-int getc(){
-	int c;
-	cout << "What number do you want C to be?";
-	cin >> c;
-	return c;
-}
-int getdescrim(int a, int b, int c){
+int QuadraticEquation::geta(){ return a; }
+int QuadraticEquation::getb(){ return b; }
+int QuadraticEquation::getc(){ return c; }
+int QuadraticEquation::getdescrim(){
 	int descrim = pow(b, 2) - 4 * a*c;
 	return descrim;
 }
-double getroot1(int a, int b, int c, int & descrim){
-	int root1 = (-b + (sqrt(descrim))) / 2 * a;
+double QuadraticEquation::getroot1(){
+	int root1 = (-b + (sqrt(pow(b,2)-4*a*c))) / 2 * a;
 	return root1;
 }
-double getroot2(int a, int b, int c, int&descrim){
-	int root2 = (-b - (sqrt(descrim))) / 2 * a;
+double QuadraticEquation::getroot2(){
+	int root2 = (-b - (sqrt(pow(b, 2) - 4 * a*c))) / 2 * a;
 	return root2;
 }
+
+
+int main(){
+	int x, y, z;
+	cout << "Give me 3 numbers" << endl;
+	cin >> x >> y >> z;
+	QuadraticEquation EQ(x,y,z);
+	
+	cout<<"a: "<<EQ.geta()<<" ";
+	cout<<"b: "<<EQ.getb()<<" ";
+	cout<<"c: "<<EQ.getc()<<" ";
+	cout<<"the descriminate is " << EQ.getdescrim() << endl;
+	if (EQ.getdescrim() < 0)
+		cout << "The equation has no real roots" << endl;
+	else if (EQ.getdescrim() == 0)
+		cout << "the only root is " << EQ.getroot1() << endl;
+	else{
+		cout << "root 1 is " << EQ.getroot1() << endl;
+		cout << "root 2 is " << EQ.getroot2() << endl;
+	}
+}
+
+
