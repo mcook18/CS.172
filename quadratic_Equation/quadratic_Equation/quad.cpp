15551fa368dbfe07b5e971d733a9a63e7906e487
mcook18 <mcnadine.cook@gmail.com>
2015-05-26 10:11:40 -0700

    hw 9.6

--- quadratic_Equation/quadratic_Equation/quad.cpp
+++ quadratic_Equation/quadratic_Equation/quad.cpp
@@ -21,13 +21,50 @@ public:
 
 };
 
+int geta();
+int getb();
+int getc();
+int getdescrim(int a, int b, int c);
+double getroot1(int a, int b, int c);
+double getroot2(int a, int b, int c);
 
 
 int main(){
-	QuadraticEquation;
+	QuadraticEquation EQ;
 
 	geta();
 	getb();
 	getc();
+	cout << getdescrim(a, b, c)
+}
 
+int geta(){
+	int a;
+	cout << "What number do you want A to be?";
+	cin >>a;
+	return a;
+}
+int getb(){
+	int b;
+	cout << "What number do you want B to be?";
+	cin >> b;
+	return b;
+}
+int getc(){
+	int c;
+	cout << "What number do you want C to be?";
+	cin >> c;
+	return c;
+}
+int getdescrim(int a, int b, int c){
+	int descrim = pow(b, 2) - 4 * a*c;
+	return descrim;
+}
+double getroot1(int a, int b, int c, int & descrim){
+	int root1 = (-b + (sqrt(descrim))) / 2 * a;
+	return root1;
+}
+double getroot2(int a, int b, int c, int&descrim){
+	int root2 = (-b - (sqrt(descrim))) / 2 * a;
+	return root2;
 }
